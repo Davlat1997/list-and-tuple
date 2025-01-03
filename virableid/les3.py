3018166de771a42cabd4286e1d8159c793e0c00d
Davlat1997 <aslamovavazira818@gmail.com>
2025-01-03 18:28:28 +0500

    done virableid 3

---
+++ virableid/les3.py
@@ -0,0 +1,11 @@
+print = input("o'zgaruvchi")  # print, o'zgaruvchi nomi sifatida ishlatilmasligi kerak
+
+777name = "Davlatshokh"  # raqam bilan boshlanmaydi
+
+first name = "Islomiy"  # bo'shliq bo'lishi mumkin emas
+
+
+
+# To'g'ri nomlash usullari:
+name_876 = "Davlatshokh"  # Raqam o'zgaruvchi oxirida ishlatilishi mumkin
+first_name = "Islomiy"  # Snake case usulida nomlash
