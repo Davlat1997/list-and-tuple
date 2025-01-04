9340ba88f27aa963d5fb1ec8c66cc285c2744baa
Davlat1997 <aslamovavazira818@gmail.com>
2025-01-04 23:07:49 +0500

    done 2

---
+++ list/les2.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Boshlang'ich ro'yxat:", numbers)
+
+numbers.append(11)
+print("Oxiriga element qo'shilgandan keyin:", numbers)
+
+numbers.pop(0)
+print("Boshlang'ich element olib tashlangandan keyin:", numbers)
