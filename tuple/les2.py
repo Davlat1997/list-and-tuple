0e54a74b3e77c18230bbae6c72f3ae07c9b53b6a
Davlat1997 <aslamovavazira818@gmail.com>
2025-01-07 13:32:32 +0500

    done 2

---
+++ tuple/les2.py
@@ -0,0 +1,8 @@
+my_tuple = (10, 20, 30, 40)
+print("tuple:", my_tuple)
+
+my_tuple = my_tuple + (50,) 
+print("Yangi tuple:", my_tuple)
+
+#Tuple o’zgarmas (immutable) bo’lgan ma’lumot turi bo’lgani uchun, unga to’g’ridan-to’g’ri yangi element qo’shib bo’lmaydi.
+#Yuqoridagi kodda qo’shish usuli ishlaydi, chunki u asl tuple o’zgartirilmaydi, balki yangi tuple yaratiladi va natija yangi tuplega saqlanadi.
