62efa2ac6535a8b0aba1e08476deb35439ca753c
Davlat1997 <aslamovavazira818@gmail.com>
2025-01-07 13:47:20 +0500

    done 3

---
+++ tupleindexing/les3.py
@@ -0,0 +1,9 @@
+my_tuple = (5, 10, 15, 20, 25, 30, 35, 40, 45, 50)
+
+element2 = my_tuple[1] 
+element8 = my_tuple[7]  
+print("2-element:", element2)
+print("8-element:", element8)
+
+result = element2 + element8
+print("Yig'indisi:", result)
