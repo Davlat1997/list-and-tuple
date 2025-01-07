09e4b80cc11f9e8b3b2af528505941234b3484c1
Davlat1997 <aslamovavazira818@gmail.com>
2025-01-07 16:37:24 +0500

    done 2

---
+++ tuplemethods/les2.py
@@ -0,0 +1,9 @@
+my_tuple = (10, 20, 30, 20, 40)
+
+count = 0
+for element in my_tuple:
+    if element == 20:
+        count += 1
+print(f"20 elementi tuple ichida {count} marta takrorlangan.")
+index_30 = my_tuple.index(30)
+print("30 elementi tuple ichida indeks:", index_30)
